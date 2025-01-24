ee4cf99efe65aeb986be53acbc5d7f48cc977e2b
Mykyta Mazurenko <surprisedsunrise@gmail.com>
2025-01-24 12:11:08 +0200

    update default value for jira issue description

--- ts/src/apps/jira/helpers/get-default-description-value.ts
+++ ts/src/apps/jira/helpers/get-default-description-value.ts
@@ -2,6 +2,7 @@ import { QorusRequest } from '@qoretechnologies/ts-toolkit';
 import { TQoreGetDefaultValueFunction } from '../../../global/models/qore';
 import { JIRA_CONN_OPTIONS } from '../constants';
 import { jiraDocumentFormatOption } from '../options/jira-document.option';
+import { Debugger } from '../../../utils/Debugger';
 
 export const getJiraIssueDescriptionDefaultValue: TQoreGetDefaultValueFunction<
   typeof JIRA_CONN_OPTIONS
@@ -11,21 +12,27 @@ export const getJiraIssueDescriptionDefaultValue: TQoreGetDefaultValueFunction<
     opts,
   } = context;
 
-  if (opts?.issueIdOrKey) {
-    const { data } = await QorusRequest.get<any>(
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
+  try {
+    if (opts?.issueIdOrKey) {
+      const { data } = await QorusRequest.get<any>(
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+          path: `/ex/jira/${cloud_id}/rest/api/3/issue/${opts.issueIdOrKey}`,
         },
-        path: `/ex/jira/${cloud_id}/rest/api/3/issue/${opts.issueIdOrKey}`,
-      },
-      { url: `https://api.atlassian.com`, endpointId: 'Jira' }
-    );
+        { url: `https://api.atlassian.com`, endpointId: 'Jira' }
+      );
 
-    if (data.fields.description) {
-      return data.fields.description;
+      if (data.fields.description) {
+        return data.fields.description;
+      }
     }
-  }
 
-  return jiraDocumentFormatOption.type.default_value;
+    return jiraDocumentFormatOption.type.default_value;
+  } catch (error) {
+    Debugger.log('Error while fetching Jira issue description', error);
+
+    return jiraDocumentFormatOption.type.default_value;
+  }
 };
