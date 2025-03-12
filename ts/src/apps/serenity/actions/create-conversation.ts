f03fa92a5bcfb04aa160dfda9d8ae7f7e0c7428a
Mykyta Mazurenko <surprisedsunrise@gmail.com>
2025-03-12 16:14:37 +0200

    add create-conversation action for Serenity app

---
+++ ts/src/apps/serenity/actions/create-conversation.ts
@@ -0,0 +1,97 @@
+import {
+  EQoreAppActionCode,
+  QoreAppCreator,
+  QorusRequest,
+  TQoreMappedOptions,
+  TQoreOptions,
+  TQoreResponseType,
+} from '@qoretechnologies/ts-toolkit';
+import { SERENITY_APP_NAME } from '../constants';
+
+const options = {
+  agentCode: {
+    required: true,
+    type: 'string',
+  },
+  userIdentifier: {
+    required: false,
+    type: 'string',
+  },
+  inputParameters: {
+    required: false,
+    type: {
+      type: 'hash',
+      fields: {
+        key: {
+          type: 'string',
+          required: true,
+        },
+        value: {
+          type: 'string',
+          required: true,
+        },
+      },
+    },
+  },
+} satisfies TQoreOptions;
+
+const response_type = {
+  type: 'hash',
+  fields: {},
+} satisfies TQoreResponseType;
+
+export const CreateSerenityConversation = QoreAppCreator.createLocalizedAction<typeof options>({
+  action: 'create-conversation',
+  app: SERENITY_APP_NAME,
+  action_code: EQoreAppActionCode.ACTION,
+  api_function: async (data, _opts, context) => {
+    const token = context?.conn_opts?.token;
+    const agentCode = data?.agentCode;
+    const parameters = data?.inputParameters || [];
+    const userIdentifier = data?.userIdentifier;
+
+    const missingValues: string[] = [];
+
+    if (!token) {
+      missingValues.push('token');
+    }
+
+    if (!agentCode) {
+      missingValues.push('agentCode');
+    }
+
+    if (missingValues.length) {
+      throw new Error(
+        `All of the following: ${missingValues.join(', ')} are required to create a Serenity conversation`
+      );
+    }
+
+    const body: Record<string, any> = {};
+
+    if (parameters?.length) {
+      body.inputParameters = parameters;
+    }
+
+    if (userIdentifier) {
+      body.userIdentifier = userIdentifier;
+    }
+
+    const response = await QorusRequest.post<{
+      data: TQoreMappedOptions<typeof response_type.fields>;
+    }>(
+      {
+        path: `/api/v2/agent/${agentCode}/conversation`,
+        data: body,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+      },
+      { endpointId: 'serenity', url: 'https://api.serenitystar.ai' }
+    );
+
+    return response?.data;
+  },
+  options,
+  response_type,
+});
