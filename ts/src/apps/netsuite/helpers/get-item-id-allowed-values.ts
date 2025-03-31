4f43c44d5f0a0b75ed1d12bb65111f17cb9a81b1
Mykyta Mazurenko <surprisedsunrise@gmail.com>
2025-03-31 15:54:18 +0300

    refactor netsuite item allowed values mapping

--- ts/src/apps/netsuite/helpers/get-item-id-allowed-values.ts
+++ ts/src/apps/netsuite/helpers/get-item-id-allowed-values.ts
@@ -17,43 +17,35 @@ type TNetsuiteItemArrayAllowedValue = {
   items: { item: { id: string } }[];
 };
 
-const mapNetSuiteItemId = (item: TNetsuiteItemData): IQoreAllowedValue => {
-  return {
-    value: item.id,
-    display_name: item.displayname || item.id,
-    desc:
-      `ID: ${item.id}\n\nName: ${item.displayname || item.id}\n\n` +
-      `Type: ${item.itemtype}\n\n` +
-      `Subtype: ${item.subtype}`,
-  };
-};
+type TMapType = 'string' | 'object' | 'array';
 
-const mapNetSuiteItem = (item: TNetsuiteItemData): IQoreAllowedValue => {
-  return {
-    value: { item: { id: item.id } },
-    display_name: item.displayname || item.id,
-    desc:
-      `ID: ${item.id}\n\nName: ${item.displayname || item.id}\n\n` +
-      `Type: ${item.itemtype}\n\n` +
-      `Subtype: ${item.subtype}`,
-  };
-};
+const getMapNetSuiteItem =
+  <T extends TNetsuiteItemAllowedValue | TNetsuiteItemArrayAllowedValue | string>(type: TMapType) =>
+  (item: TNetsuiteItemData): IQoreAllowedValue<T> => {
+    let value: T;
 
-const mapNetSuiteItemArray = (item: TNetsuiteItemData): IQoreAllowedValue => {
-  return {
-    value: { items: [{ item: { id: item.id } }] },
-    display_name: item.displayname || item.id,
-    desc:
-      `ID: ${item.id}\n\nName: ${item.displayname || item.id}\n\n` +
-      `Type: ${item.itemtype}\n\n` +
-      `Subtype: ${item.subtype}`,
+    if (type === 'array') {
+      value = { items: [{ item: { id: item.id } }] } as T;
+    } else if (type === 'object') {
+      value = { item: { id: item.id } } as T;
+    } else {
+      value = item.id as T;
+    }
+
+    return {
+      value,
+      display_name: item.displayname || item.id,
+      desc:
+        `ID: ${item.id}\n\nName: ${item.displayname || item.id}\n\n` +
+        `Type: ${item.itemtype}\n\n` +
+        `Subtype: ${item.subtype}`,
+    };
   };
-};
 
 const createNetsuiteItemIdAllowedValuesFunction = <
   T extends TNetsuiteItemAllowedValue | TNetsuiteItemArrayAllowedValue | string,
 >(
-  type: 'id' | 'item' | 'array',
+  type: TMapType,
   itemSubtype?: 'Sale' | 'Purchase'
 ): TQoreGetAllowedValuesFunction<typeof NETSUITE_CONN_OPTIONS, T> => {
   return async (context): Promise<IQoreAllowedValue<T>[]> => {
@@ -64,19 +56,10 @@ const createNetsuiteItemIdAllowedValuesFunction = <
       throw new Error('The token and account_id are required to get NetSuite item allowed values');
     }
 
-    let mapItem: ((item: TNetsuiteItemData) => IQoreAllowedValue) | undefined = undefined;
-    if (type === 'item') {
-      mapItem = mapNetSuiteItem;
-    } else if (type === 'array') {
-      mapItem = mapNetSuiteItemArray;
-    } else if (type === 'id') {
-      mapItem = mapNetSuiteItemId;
-    }
-
     const items = await fetchNetsuiteAllowedValues({
       account_id,
       token,
-      mapItemToAllowedValue: mapItem || mapNetSuiteItemId,
+      mapItemToAllowedValue: getMapNetSuiteItem<T>(type),
       query:
         `SELECT * FROM item WHERE item.isinactive='F' ` +
         `AND (item.subtype='${itemSubtype}' OR item.subtype='Both') ORDER BY item.lastmodifieddate DESC`,
@@ -89,12 +72,12 @@ const createNetsuiteItemIdAllowedValuesFunction = <
 export const getNetsuiteSalesItemIdAllowedValues: TQoreGetAllowedValuesFunction<
   typeof NETSUITE_CONN_OPTIONS,
   string
-> = createNetsuiteItemIdAllowedValuesFunction<string>('id', 'Sale');
+> = createNetsuiteItemIdAllowedValuesFunction<string>('string', 'Sale');
 
 export const getNetsuiteSalesItemIdObjectAllowedValues: TQoreGetAllowedValuesFunction<
   typeof NETSUITE_CONN_OPTIONS,
   TNetsuiteItemAllowedValue
-> = createNetsuiteItemIdAllowedValuesFunction<TNetsuiteItemAllowedValue>('item', 'Sale');
+> = createNetsuiteItemIdAllowedValuesFunction<TNetsuiteItemAllowedValue>('object', 'Sale');
 
 export const getNetsuiteSalesItemIdArrayAllowedValues: TQoreGetAllowedValuesFunction<
   typeof NETSUITE_CONN_OPTIONS,
@@ -104,7 +87,7 @@ export const getNetsuiteSalesItemIdArrayAllowedValues: TQoreGetAllowedValuesFunc
 export const getNetsuitePurchaseItemIdAllowedValues: TQoreGetAllowedValuesFunction<
   typeof NETSUITE_CONN_OPTIONS,
   TNetsuiteItemAllowedValue
-> = createNetsuiteItemIdAllowedValuesFunction<TNetsuiteItemAllowedValue>('item', 'Purchase');
+> = createNetsuiteItemIdAllowedValuesFunction<TNetsuiteItemAllowedValue>('string', 'Purchase');
 
 export const getNetsuitePurchaseItemIdArrayAllowedValues: TQoreGetAllowedValuesFunction<
   typeof NETSUITE_CONN_OPTIONS,
