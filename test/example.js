2d08ac05ba5d62ff887ec5f3fee15917668ea7e5
David Nichols <david@qore.org>
2025-05-06 19:15:25 +0200

    added support for google apps

--- test/example.js
+++ test/example.js
@@ -1048,5 +1048,43 @@ exports.actionsCatalogue = {
                 };
             },
         });
+
+        api.registerApp({
+            "name": "gmail-test",
+            "display_name": "JavaScript Gmail Test",
+            "short_desc": "Gmail test",
+            "desc": "Gmail test",
+            // "logo" is a base64-encoded string
+            "logo": "PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiIHN0YW5kYWxvbmU9Im5vIj8+CjwhRE9DVFlQRSBzdmcgUFVCTElDICItLy9XM0MvL0RURCBTVkcgMS4xLy9FTiIgImh0dHA6Ly93d3cudzMub3JnL0dyYXBoaWNzL1NWRy8xLjEvRFREL3N2ZzExLmR0ZCI+Cjxzdmcgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDUyIDYzIiB2ZXJzaW9uPSIxLjEiIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgeG1sbnM6eGxpbms9Imh0dHA6Ly93d3cudzMub3JnLzE5OTkveGxpbmsiIHhtbDpzcGFjZT0icHJlc2VydmUiIHhtbG5zOnNlcmlmPSJodHRwOi8vd3d3LnNlcmlmLmNvbS8iIHN0eWxlPSJmaWxsLXJ1bGU6ZXZlbm9kZDtjbGlwLXJ1bGU6ZXZlbm9kZDtzdHJva2UtbGluZWpvaW46cm91bmQ7c3Ryb2tlLW1pdGVybGltaXQ6MjsiPgogICAgPGcgdHJhbnNmb3JtPSJtYXRyaXgoMSwwLDAsMSwtMTYuNjUsLTIzLjAxNzIpIj4KICAgICAgICA8cGF0aCBkPSJNNjguMzYzLDYzLjk3M0w2OC4zNjMsNDAuMTA5QzY4LjM2Myw0MC4xMDkgNjguMzYzLDM3LjExMyA2NS43NjgsMzUuNjE1TDQ1LjEwMiwyMy42ODNDNDUuMTAyLDIzLjY4MyA0Mi41MDcsMjIuMTg1IDM5LjkxMiwyMy42ODNMMTkuMjQ1LDM1LjYxNUMxOS4yNDUsMzUuNjE1IDE2LjY1LDM3LjExMyAxNi42NSw0MC4xMDlMMTYuNjUsNjMuOTczQzE2LjY1LDYzLjk3MyAxNi42NSw2Ni45NjkgMTkuMjQ1LDY4LjQ2N0w0Ny44MzksODQuODIyQzQ3LjgzOSw4NC44MjIgNTAuNDM0LDg2LjM2OCA1My4wMjksODQuODdMNjQuNjUyLDc4LjExMkw0Mi41Miw2NS41MDNMNDIuNTA3LDY1LjUxMUwzMC44NDMsNTguNzc2TDMwLjg0Myw0NS4zMDdMNDIuNTA3LDM4LjU3M0w1NC4xNzEsNDUuMzA3TDU0LjE3MSw1OC43NzZMNDUuMjEzLDYzLjk0OEw1OS41NjUsNzIuMDVMNjUuNzY4LDY4LjQ2OUM2NS43NjksNjguNDY4IDY4LjM2Myw2Ni45NyA2OC4zNjMsNjMuOTczIiBzdHlsZT0iZmlsbDpyZ2IoNjQsNjQsNjQpO2ZpbGwtcnVsZTpub256ZXJvOyIvPgogICAgPC9nPgo8L3N2Zz4K",
+            "logo_file_name": "gmail-test.svg",
+            "logo_mime_type": "image/svg+xml",
+            "google_app": {
+                "api": "gmail",
+            },
+            "rest": {
+                "data": "json",
+                "oauth2_auth_url": "https://accounts.google.com/o/oauth2/v2/auth",
+                "oauth2_client_id": "x",
+                "oauth2_client_secret": "y",
+                "oauth2_grant_type": "authorization_code",
+                "oauth2_token_url": "https://oauth2.googleapis.com/token",
+                "url": "tsrest-gmail-test://www.googleapis.com",
+            },
+        });
+
+        api.registerAction({
+            "app": "gmail-test",
+            "action": "send-email",
+            "display_name": "Send Email",
+            "short_desc": "Send email",
+            "desc": "Send email",
+            "action_code": 2,  // DPAT_API == 2
+            "google_action": {
+                "resource": "users/messages/send",
+                "path_args": {
+                    "userId": "me",
+                },
+            },
+        });
     }
 };
