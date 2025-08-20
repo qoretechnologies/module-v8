13713ee4cd51d48f786b678e0572569ff8ba71f5
Mykyta Mazurenko <surprisedsunrise@gmail.com>
2025-08-20 19:20:05 +0300

    add zoom meeting summary trigger

--- ts/src/apps/zoom/triggers/index.ts
+++ ts/src/apps/zoom/triggers/index.ts
@@ -1,2 +1,3 @@
 export { default as ZoomNewMeetingTrigger } from './new-meeting.trigger';
 export { default as ZoomNewWebinarTrigger } from './new-webinar.trigger';
+export { default as ZoomNewMeetingSummary } from './new-meeting-summary.trigger';
