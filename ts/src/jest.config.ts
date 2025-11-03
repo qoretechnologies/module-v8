bfb9524c84dc6bfcf16981be0e51387726ea6365
Mykyta Mazurenko <surprisedsunrise@gmail.com>
2025-11-03 11:54:09 +0200

    update test:ci script to include CI flag and maxWorkers configuration

--- ts/src/jest.config.ts
+++ ts/src/jest.config.ts
@@ -3,6 +3,7 @@ import { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  maxWorkers: '50%',
   coverageReporters: ['json-summary', 'text', 'lcov'],
   testTimeout: 60000,
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
